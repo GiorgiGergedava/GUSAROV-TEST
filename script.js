299f58836ef834b0da69a3db91c9d6dedd6ba599
GiorgiGergedava <giogergedava123@gmail.com>
2024-08-12 16:40:45 +0400

    Add.index,reset,style,mobile,js
    I created a test sample from the provided figma, where I used JavaScript granularity in HTML and CSS and adapted it to mobile devices.

---
+++ script.js
@@ -0,0 +1,17 @@
+// let burger = document.querySelector(".burger");
+// let mobilemenu = document.querySelector(".header_nav");
+
+// burger.addEventListener("click hover", () => {
+//     mobilemenu.classList.add("show")
+//     burger.style.display = "none"
+//     xmark.style.display = "block";
+
+// })
+
+
+// xmark.addEventListener("click", () => {
+//     mobilemenu.classList.remove("show")
+//     burger.style.display = "block"
+//     xmark.style.display = "none";
+
+// })
